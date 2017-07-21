6aa687b403e888b815007d1b2c297db46b4c69dd
Susanna Zandonai <susanna.zandonai@gmail.com>
2017-07-21 20:13:55 +0100

    first commit, div positioning

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-var bool = true;
+jQuery(document).ready(function(){
+	console.log('il DOM è pronto');
+	var i = 0;
 
-var stampa = function(val) {
-	console.log("chiamata", val);
-	if (val) {
-		var divRosso = document.getElementById('rosso');
+	var handleWindowResize = function() {
+		console.log(window.innerWidth);
+		i = i + 1;
 		var divBlue = document.getElementById('blu');
+		divBlue.style.top = i+'px';
+	};
 
-		divRosso.style.backgroundColor = 'blue';
-		divBlue.style.backgroundColor = 'red';
-	} else {
-		var divRosso = document.getElementById('rosso');
-		var divBlue = document.getElementById('blu');
-
-		divRosso.style.backgroundColor = 'red';
-		divBlue.style.backgroundColor = 'blue';
-	}
-}
-
-stampa(bool);
-//
-setTimeout(function() {
-	bool = false;
-	stampa(bool);
-}, 1000);
-//
-setTimeout(function() {
-	bool = true;
-	stampa(bool);
-}, 2000);
-//
-setTimeout(function() {
-	bool = false;
-	stampa(bool);
-}, 3000);
+	window.addEventListener('resize', handleWindowResize, true);
+});
