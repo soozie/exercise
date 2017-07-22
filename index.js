ee9f20a3dd8e775886475a053f5ea98eaf36e926
Susanna Zandonai <susanna.zandonai@gmail.com>
2017-07-22 15:06:04 +0100

    Restored first app

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-jQuery(document).ready(function(){
-	console.log('Dom is ready!');
-	var i = 0;
+var bool = true;
 
-	var handleWindowResize = function() {
-		console.log("Resizing...");
-	};
+var stampa = function(val) {
+	console.log("chiamata", val);
+	if (val) {
+		var divRosso = document.getElementById('rosso');
+		var divBlue = document.getElementById('blu');
 
-	window.addEventListener('resize', handleWindowResize, true);
-});
+		divRosso.style.backgroundColor = 'blue';
+		divBlue.style.backgroundColor = 'red';
+	} else {
+		var divRosso = document.getElementById('rosso');
+		var divBlue = document.getElementById('blu');
+
+		divRosso.style.backgroundColor = 'red';
+		divBlue.style.backgroundColor = 'blue';
+	}
+}
+
+stampa(bool);
+//
+setTimeout(function() {
+	bool = false;
+	stampa(bool);
+}, 1000);
+//
+setTimeout(function() {
+	bool = true;
+	stampa(bool);
+}, 2000);
+//
+setTimeout(function() {
+	bool = false;
+	stampa(bool);
+}, 3000);
